f3540366e437fb943913e3ea1176f2d7b869b8bd
root <root@testadmin.genians.com>
2022-11-15 17:28:42 +0900

    기본 컨셉 확인 및 esxi 서버 통신 테스트

--- update_vm_status.py
+++ update_vm_status.py
@@ -1,25 +1,14 @@
 from mysql_lib import MysqlController
-import re
+import vm_utils
 
-#app.conf 파일에 있는 db정보를 가져오는 함수
-def readDatabaseInfo():
-    db_info = {}
-    #정규식을 이용하여 필요한 정보만 dictionary array에 저장하기 위해 정규식 설정
-    re_match = re.compile('^db')
-    f = open('/etc/app.conf')
-    readText = f.readlines()
-    
-    for line in readText:
-        #db로 시작하는 line이면 key와 value로 split 후 사용
-        if re_match.match(line):
-            split_text = line.split("=")
-            db_info[split_text[0]] = split_text[1].strip('\n')
-    return db_info
+# app_info = readAppInfo()
+# db_controller = MysqlController(app_info)
+# query = "select * from information_schema.schemata" #테스트용 쿼리
+# db_cursor = db_controller.query_executor(query)
+#def all_vm_list(server_list):
 
-db_connect_info = readDatabaseInfo()
-db_controller = MysqlController(db_connect_info)
-query = "select * from information_schema.schemata" #테스트용 쿼리
-db_cursor = db_controller.query_executor(query)
+info = vm_utils.readAppInfo() 
+print(vm_utils.power_on_vm_list(info["server_ip"]))
 
-for raw in db_cursor:
-    print(raw)
+
+
---
+++ vm_utils.py
@@ -0,0 +1,36 @@
+import re
+import subprocess
+
+#app.conf 파일에 있는 db정보를 가져오는 함수
+def readAppInfo(want = all):
+    app_info = {}
+    #정규식을 이용하여 필요한 정보만 dictionary array에 저장하기 위해 정규식 설정
+    db_match = re.compile('^db')
+    server_match = re.compile('^server')
+    f = open('/etc/app.conf')
+    readText = f.readlines()
+    
+    for line in readText:
+        #db로 시작하는 line이면 key와 value로 split 후 사용
+        if db_match.match(line) and (want == 'db' or want == 'all'):
+            split_text = line.split("=")
+            app_info[split_text[0]] = split_text[1].strip('\n')
+
+        elif server_match.match(line) and (want == 'server_ip' or want == 'all'):
+            split_text = line.split("=")
+            server_ip = split_text[1].split(",")
+            app_info[split_text[0]] = server_ip
+            app_info[split_text[0]][-1] = app_info[split_text[0]][-1].strip('\n')
+
+    return app_info
+
+# process list를 조회하여 power on 상태의 vm list 조회
+def power_on_vm_list(server_list):
+    list = []
+
+    for server in server_list:
+        test = subprocess.check_output("ssh root@%s esxcli vm process list | grep \"Display Name\" | awk -F': ' '{print $2}'" % server, shell=True).decode('utf-8')
+        list += test.split('\n')
+        list.remove('')
+    
+    return list
